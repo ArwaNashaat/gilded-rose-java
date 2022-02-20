cb47629800ac26f8ec01a003ee3d38080346b7de
ArwaNashaat <arwanashaat@gmail.com>
2022-02-20 14:04:00 +0200

    check that sellIn & quality values decrease after updating quality

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,13 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void sellInAndQualityValuesDecreaseAfterUpdate() {
+        Item[] items = new Item[] { new Item("foo", 1, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
 }
