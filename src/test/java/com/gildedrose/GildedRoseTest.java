c8d9f4a3300e1e0ec293c73f34ec699c08b120ff
ArwaNashaat <arwanashaat@gmail.com>
2022-02-20 14:09:25 +0200

    check that the quality of an item is never negative

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -31,4 +31,19 @@ class GildedRoseTest {
         assertEquals(4, app.items[0].quality);
     }
 
+    @Test
+    void whenSellInIsNotEqualZeroTheQualityOfAnItemIsNeverNegative() {
+            Item[] items = new Item[] { new Item("foo", 1, 0) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(0, app.items[0].quality);
+    }
+    @Test
+    void whenSellInEqualsZeroTheQualityOfAnItemIsNeverNegative() {
+            Item[] items = new Item[] { new Item("foo", 0, 1) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(0, app.items[0].quality);
+    }
+
 }
