edc9c83246cc99e807d1c7ee9d5fc9cd6eb7ed4b
ArwaNashaat <arwanashaat@gmail.com>
2022-02-22 19:39:25 +0200

    update dummy test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,14 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void checkNameAfterUpdatingQuality() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
-
-    @Test
     void sellInAndQualityValuesDecreaseAfterUpdate() {
         Item[] items = new Item[]{new Item("foo", 1, 1)};
         GildedRose app = new GildedRose(items);
@@ -30,7 +22,6 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(4, app.items[0].quality);
     }
-
 
     @Test
     void qualityOfNormalItemDecreasesQualityIs49() {
