b4f82f9851e0493efd95666c1fea12ecdadd87df
ArwaNashaat <arwanashaat@gmail.com>
2022-02-25 13:14:22 +0200

    add normal item to the database

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,12 +1,42 @@
 package com.gildedrose;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "Item")
 public class Item {
 
+    @Id
+    @GeneratedValue
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    public int id;
+
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     public String name;
 
+    @Column(
+            name = "sell_in",
+            nullable = false
+    )
     public int sellIn;
 
+    @Column(
+            name = "quality",
+            nullable = false
+    )
     public int quality;
+
+    public Item() {
+    }
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
---
+++ src/test/java/com/gildedrose/database/ItemTest.java
@@ -0,0 +1,29 @@
+package com.gildedrose.database;
+
+import com.gildedrose.Item;
+import com.gildedrose.ItemRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ItemTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    public void addNormalItem() {
+        Item item = new Item("foo", 5, 5);
+        itemRepository.save(item);
+
+        assertEquals(item.toString(), itemRepository.getById(1).toString());
+    }
+}
